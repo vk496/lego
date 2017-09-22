0ebc94e5bf39977cb5033271969ec1b4fe6ca3e1
Valentin <vk18496@gmail.com>
2017-09-22 15:31:31 +0200

    Improvements

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 trap 'exit 130' INT #Exit if trap Ctrl+C
 
-docker-compose build && docker-compose up #Deploy services
+docker-compose build && docker-compose up || exit 1 #Deploy services
 
 sudo sysctl -w net.ipv4.ip_forward=1 #Enable IP Forwarding
 
@@ -12,12 +12,12 @@ while read -r line; do
 	if [[ $line ]]; then
 	    com="${com}|grep $line "
 	fi
-done < <(docker network ls | grep $(basename $(pwd)) | awk '{print $1}')
+done < <(docker network ls | grep "$(basename "$(pwd)")" | awk '{print $1}')
 
 #Disable iptables for comunicate between virtual interfaces
 if [[ $com ]]; then
 	while read -r line; do
-	    echo iptables ${line/-A DOCKER-ISOLATION/-D DOCKER-ISOLATION}
+	    echo "iptables ${line/-A DOCKER-ISOLATION/-D DOCKER-ISOLATION}"
 	    eval "sudo iptables ${line/-A /-D }"
 	done < <(eval "sudo iptables-save | ${com:1:-1}")
 fi
