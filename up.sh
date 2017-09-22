fe852db249b50ed4dd403e40e319360ad9682579
Valentin <vk18496@gmail.com>
2017-09-22 18:26:08 +0200

    init snmp

--- up.sh
+++ up.sh
@@ -2,7 +2,7 @@
 set -e
 trap 'exit 130' INT #Exit if trap Ctrl+C
 
-docker-compose build && docker-compose up || exit 1 #Deploy services
+docker-compose build && docker-compose up -d || exit 1 #Deploy services
 
 sudo sysctl -w net.ipv4.ip_forward=1 #Enable IP Forwarding
 
@@ -10,7 +10,7 @@ sudo sysctl -w net.ipv4.ip_forward=1 #Enable IP Forwarding
 com=
 while read -r line; do
 	if [[ $line ]]; then
-	    com="${com}|grep $line "
+	    com="${com}\|$line"
 	fi
 done < <(docker network ls | grep "$(basename "$(pwd)")" | awk '{print $1}')
 
@@ -18,6 +18,6 @@ done < <(docker network ls | grep "$(basename "$(pwd)")" | awk '{print $1}')
 if [[ $com ]]; then
 	while read -r line; do
 	    echo "iptables ${line/-A DOCKER-ISOLATION/-D DOCKER-ISOLATION}"
-	    eval "sudo iptables ${line/-A /-D }"
-	done < <(eval "sudo iptables-save | ${com:1:-1}")
+	    eval "sudo iptables ${line/-A DOCKER-ISOLATION/-D DOCKER-ISOLATION}"
+	done < <(eval "sudo iptables-save | grep \"DOCKER-ISOLATION\" | grep -i \"\-j DROP\" | grep -v \"docker0\" | grep \"${com:2:-1}\"")
 fi
