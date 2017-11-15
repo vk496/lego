8a316e6c6e3620c1055ebb2713b5dde803730cb5
Valentin <vk18496@gmail.com>
2017-11-15 15:45:23 +0100

    Edit LDAP + Fix Owncloud LDAP

--- um/owncloud/misc/owncloud_ldap.sh
+++ um/owncloud/misc/owncloud_ldap.sh
@@ -11,9 +11,13 @@ su - www-data -s /bin/bash -c "cd /var/www/owncloud/ && \
                                                         php occ ldap:set-config '' ldapAgentName cn=admin,dc=um,dc=es &&\
                                                         php occ ldap:set-config '' ldapAgentPassword um_password &&\
                                                         php occ ldap:set-config '' ldapBase dc=um,dc=es &&\
-                                                        php occ ldap:set-config '' ldapUserFilter '(|(objectclass=inetOrgPerson))' &&\
-                                                        php occ ldap:set-config '' ldapLoginFilter '(&(|(objectclass=inetOrgPerson))(|(uid=%uid)(|(mailPrimaryAddress=%uid)(mail=%uid))))' &&\
+                                                        php occ ldap:set-config '' ldapUserFilter '(&(|(objectclass=inetOrgPerson))(|(memberof=cn=owncloud,ou=Servicios,dc=um,dc=es)))' &&\
+                                                        php occ ldap:set-config '' ldapLoginFilter '(&(&(|(objectclass=inetOrgPerson))(|(memberof=cn=owncloud,ou=Servicios,dc=um,dc=es)))(|(cn=%uid)(|(mailPrimaryAddress=%uid)(mail=%uid))))' &&\
                                                         php occ ldap:set-config '' ldapLoginFilterEmail 1 &&\
                                                         php occ ldap:set-config '' ldapUserDisplayName cn &&\
                                                         php occ ldap:set-config '' ldapUserFilterObjectclass inetOrgPerson &&\
-                                                        php occ ldap:set-config '' ldapConfigurationActive 1"
+                                                        php occ ldap:set-config '' ldapConfigurationActive 1 &&\
+                                                        php occ ldap:set-config '' hasMemberOfFilterSupport 1 &&\
+                                                        php occ ldap:set-config '' ldapUserFilterGroups owncloud &&\
+                                                        php occ ldap:set-config '' ldapBaseGroups dc=um,dc=es &&\
+                                                        php occ ldap:set-config '' ldapBaseUsers dc=um,dc=es"
