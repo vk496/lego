d77cf09f569213b64f953c075169b22e234c4ab1
Valentin <vk18496@gmail.com>
2017-11-30 22:55:11 +0100

    Full PKI infraestructure

--- up.sh
+++ up.sh
@@ -54,17 +54,24 @@ echo -e "Usign $color${driver}\033[0m network driver"
 sleep 1.5
 
 
-domains=( "um:192.168.251" "upm:192.168.252" )
+domains=(
+    "um:192.168.251"
+    "upm:192.168.252"
+)
 
 for domain in "${domains[@]}"; do
-    dom=$(echo $domain | cut -d: -f1) #Get domain
-    ip_range=$(echo $domain | cut -d: -f2) #Get IP range
-
+        dom=$(echo $domain | cut -d: -f1) #Get domain
+        ip_range=$(echo $domain | cut -d: -f2) #Get IP range
+        
     if ! sudo -u $SUDO_USER docker volume inspect $dom-certs 2>/dev/null >/dev/null; then
+        init_TLS=true
+        
         sudo -u $SUDO_USER docker volume create $dom-certs #All certs
         
         #Generate CA
         sudo -u $SUDO_USER docker run --rm -v $dom-certs:/certs \
+            -e CA_KEY="$dom-key.pem" \
+            -e CA_CERT="$dom.pem" \
             -e CA_SUBJECT="$dom" \
             -e CA_EXPIRE="1000" \
             -e SSL_SIZE="4096" \
@@ -75,7 +82,7 @@ for domain in "${domains[@]}"; do
 
         #Delete stuff
         echo "DELETE default server cert & keys"
-        sudo -u $SUDO_USER docker run --rm -v $dom-certs:/certs -it paulczar/omgwtfssl bash -c 'rm delete*'
+        sudo -u $SUDO_USER docker run --rm -v $dom-certs:/certs paulczar/omgwtfssl bash -c 'rm delete*'
         echo "OK"
         
         servicios=(
@@ -87,6 +94,9 @@ for domain in "${domains[@]}"; do
             subdom=$(echo $servicio | cut -d: -f1) #Get domain of service
             ip_serv=$(echo $servicio | cut -d: -f2) #Get ip of service
             
+            sudo -u $SUDO_USER docker volume create $dom-certs-$subdom #Service certs
+            
+            #Generate cert
             sudo -u $SUDO_USER docker run --rm -v $dom-certs:/certs \
                 -e SSL_KEY="$subdom-key.pem" \
                 -e SSL_CSR="$subdom-key.csr" \
@@ -96,11 +106,23 @@ for domain in "${domains[@]}"; do
                 -e SSL_SUBJECT="$subdom.$dom.es" \
                 -e SSL_IP="$ip_serv" \
             paulczar/omgwtfssl
+            
+            #Aislate the keys from CA
+            sudo -u $SUDO_USER docker run --rm -v $dom-certs:/certs -v $dom-certs-$subdom:/service_certs paulczar/omgwtfssl bash -c "cp /certs/{$subdom*,$dom.pem} /service_certs"
         done
-        
     fi
         
 done
+
+if [ $init_TLS ]; then
+    sudo -u $SUDO_USER docker volume create lego_ca #Public CA keys
+
+    for domain in "${domains[@]}"; do
+        dom=$(echo $domain | cut -d: -f1) #Get domain
+        
+        sudo -u $SUDO_USER docker run --rm -v $dom-certs:/certs -v lego_ca:/public_ca paulczar/omgwtfssl bash -c "cp /certs/$dom.pem /public_ca && cp /certs/$dom.pem /public_ca/\$(openssl x509 -in /certs/$dom.pem -noout -hash).0"
+    done
+fi
 
 sudo -u $SUDO_USER docker-compose up --build -d #Deploy services
 
